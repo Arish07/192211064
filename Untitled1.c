1e64faf38841750a4fe033c8aa2542d81f517855
Arish07 <62480652+Arish07@users.noreply.github.com>
2023-01-09 13:40:43 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float n,a,c;
+	printf("enter the number");
+	scanf("%f",&n);
+	a=n*n;
+	c=n*n*n;
+	printf("\n square of the given number is:%f",a);
+	printf("\n cube of the given number is:%f",c);
+	return 0;
+}
