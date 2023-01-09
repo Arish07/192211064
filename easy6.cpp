b9c4d48e9529009bdf24ddc2592db3bb47fa07c3
Arish07 <62480652+Arish07@users.noreply.github.com>
2023-01-09 14:05:10 +0530

    Add files via upload

---
+++ easy6.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int i,j;
+	j=18-i;
+	printf("enter your age: ");
+	scanf("%d",&i);
+	if(i>=18)
+	printf("Your able to vote");
+	else 
+	
+	printf("your allowed to vote after %d",j);
+}
